302b3969782be549ad2bf4ab1b2ceda4ffacbe17
Christopher Sparrowgrove <admin@3xsecurityservces.com>
2011-10-25 23:01:51 -0600

    Inital Upload

---
+++ updates.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+ <head>
+     <title>Updates To CMS</title>
+     <meta name="author" content="Christopher Sparrowgrove" />
+     <meta name="keyword" content="USO, usocms, christopher sparrowgrove, cms script, fort carson, United Service Orgination" />
+     <meta name="description" content="USOCMS is a Content Management System (CMS) for displaying information such as food menu, driving direction, acctivities, hours of operation, and more." />
+     <meta name="ROBOTS" content="NOINDEX,NOFOLLOW" />
+ </head>
+
+ <body>
+     <h3>Version 5.0 (October 22, 2011)</h3>
+     <ul>
+         <li>5 Security Updates</li>
+         <li>Fixed Activities Broken Link</li>
+         <li>Fixed Login Error</li>
+         <li>Added Hours Of Operation</li>
+         <li>Added Op Hours Updater To Admin Pannel</li>
+         <li>Rewrote Login Script</li>
+         <li>Rewrote Navigation Links Script</li>
+         <li>Rewrote Configuration Script</li>
+         <li>Rewrote Menu Page</li>
+         <li>Merged Logout, Login, Registration Script</li>
+         <li>Moved Visitor Logging to database</li>
+     </ul>
+     <h3>Version 4.5.0 (October 20, 2011)</h3>
+     <ul>
+         <li>Security Update</li>
+     </ul>
+     <h3>Version 4.3.3 (October 17, 2011)</h3>
+     <ul>
+         <li>Offical Release</li>
+     </ul>
+ </body>
+</html>
