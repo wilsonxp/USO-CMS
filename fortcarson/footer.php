d66926df9e095baf3056d3ac61e82f2359c246a8
Christopher Sparrowgrove <admin@3xsecurityservces.com>
2011-10-27 03:36:58 -0600

    Added New Link

--- fortcarson/footer.php
+++ fortcarson/footer.php
@@ -7,16 +7,16 @@ if (isset ($_SESSION['username']))
     $uri   = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
         if ($uri=='/fortcarson/ul')
         {
-            $links="<a href='../index.php?page=menu'>Food Menu</a> | <a href='../index.php?page=directions'>Directions</a> | <a href='../index.php?page=activities'>Activities</a> | <a href='../index.php?page=ophours'>Hours of Operation</a> | Contact Us | <a href='../index.php?dir=ul'>Admin Pannel</a> | <a href='members.php?cmd=logout'>Logout</a>";   
+            $links="<a href='../index.php?page=menu'>Food Menu</a> | <a href='../index.php?page=directions'>Directions</a> | <a href='../index.php?page=activities'>Activities</a> | <a href='../index.php?page=ophours'>Hours of Operation</a> | Contact Us  | <a href='index.php?page=privacy'>Privacy</a> | <a href='../index.php?dir=ul'>Admin Pannel</a> | <a href='members.php?cmd=logout'>Logout</a>";   
         }
         else
         {
-            $links="<a href='index.php?page=menu'>Food Menu</a> | <a href='index.php?page=directions'>Directions</a> | <a href='index.php?page=activities'>Activities</a> | <a href='index.php?page=ophours'>Hours of Operation</a> | Contact Us | <a href='index.php?dir=ul'>Admin Pannel</a> | <a href='index.php?cmd=logout'>Logout</a>";
+            $links="<a href='index.php?page=menu'>Food Menu</a> | <a href='index.php?page=directions'>Directions</a> | <a href='index.php?page=activities'>Activities</a> | <a href='index.php?page=ophours'>Hours of Operation</a> | Contact Us  | <a href='index.php?page=privacy'>Privacy</a> | <a href='index.php?dir=ul'>Admin Pannel</a> | <a href='index.php?cmd=logout'>Logout</a>";
         }
 }
 else
 {
-    $links="<a style='text-decoration:none;' href='index.php?cmd=login'>*</a> <a href='index.php?page=menu'>Food Menu</a> | <a href='index.php?page=directions'>Directions</a> | <a href='index.php?page=activities'>Activities</a> | <a href='index.php?page=ophours'>Hours of Operation</a> | Contact Us <a  style='text-decoration:none;' href='index.php?cmd=login'>*</a>";  
+    $links="<a style='text-decoration:none;' href='index.php?cmd=login'>*</a> <a href='index.php?page=menu'>Food Menu</a> | <a href='index.php?page=directions'>Directions</a> | <a href='index.php?page=activities'>Activities</a> | <a href='index.php?page=ophours'>Hours of Operation</a> | Contact Us | <a href='index.php?page=privacy'>Privacy</a> <a  style='text-decoration:none;' href='index.php?cmd=login'>*</a>";  
    
 }
 
